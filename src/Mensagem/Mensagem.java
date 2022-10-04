f3c8d4900eb96833af3a9e031775c93e4085235d
pacasarini <paulacasarini@hotmail.com>
2022-10-04 01:26:24 -0300

    enhaced switch

--- src/Mensagem/Mensagem.java
+++ src/Mensagem/Mensagem.java
@@ -4,34 +4,13 @@ public class Mensagem {
     public static void obterMensagem(int hora){
 
         switch (hora){
-            case 24:
-            case 1:
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-            case 9:
-            case 10:
-            case 11:
-            case 12:
+            case 24, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12:
                 mensagemBomDia();
                 break;
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-            case 17:
+            case 13, 14, 15, 16, 17:
                 mensagemBoaTarde();
                 break;
-            case 18:
-            case 19:
-            case 20:
-            case 21:
-            case 22:
-            case 23:
+            case 18, 19, 20, 21, 22, 23:
                 mensagemBoaNoite();
                 break;
             default:
