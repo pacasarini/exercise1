9946404204635b1883d5fe39b15f56ab2483a3ab
pacasarini <paulacasarini@hotmail.com>
2022-10-03 23:19:05 -0300

    calculadora - soma, subtracao, multiplicacao e divisao

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -2,9 +2,31 @@ package Calculadora;
 
 public class Calculadora {
 
+    //soma
     public static void soma(double numero1, double numero2){
         double resultado = numero1 + numero2;
 
-        System.out.println("a soma do " + numero1 + " + o " + numero2 + " é igual a " + resultado);
+        System.out.println("a soma de " + numero1 + " + " + numero2 + " é igual a " + resultado);
+    }
+
+    //subtraçao
+    public static void subtracao(double numero1, double numero2){
+        double resultado = numero1 - numero2;
+
+        System.out.println("a subtração de " + numero1 + " - " + numero2 + " é igual a " + resultado);
+    }
+
+    //multiplicação
+    public static void multiplicacao(double numero1, double numero2){
+        double resultado = numero1 * numero2;
+
+        System.out.println("a multiplicação de " + numero1 + " por " + numero2 + " é igual a " + resultado);
+    }
+
+    //divisão
+    public static void divisao(double numero1, double numero2) {
+        double resultado = numero1 / numero2;
+
+        System.out.println("a divisão de " + numero1 + " por " + numero2 + " é igual a " + resultado);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
         //Calculadora
         System.out.println("Exercício Calculadora");
         Calculadora.soma(3, 6);
+        Calculadora.subtracao(2, 1);
+        Calculadora.multiplicacao(3, 3);
+        Calculadora.divisao(6, 3);
+
 
     }
 }
