e0649c9bb3c6d4ca918e284e697c471e9db8eba7
pacasarini <paulacasarini@hotmail.com>
2022-10-03 23:05:19 -0300

    calculator - sum

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+package Calculadora;
+
+public class Calculadora {
+
+    public static void soma(double numero1, double numero2){
+        double resultado = numero1 + numero2;
+
+        System.out.println("a soma do " + numero1 + " + o " + numero2 + " é igual a " + resultado);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Calculadora.Calculadora;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Calculadora
+        System.out.println("Exercício Calculadora");
+        Calculadora.soma(3, 6);
+
+    }
+}
